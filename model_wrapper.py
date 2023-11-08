97a15f4d97ab6b33c3e604bb663c7b5d7703cbcc
jesse12shen <143554764+jesse12shen@users.noreply.github.com>
2023-11-08 18:38:06 -0500

    Added sigmoid dependency

--- model_wrapper.py
+++ model_wrapper.py
@@ -15,7 +15,7 @@ text2glove.py performs the role of transforming a review into a collection
 of GLoVe values and records duplicates while excluding any words outside of
 the GLoVe dictionary.
 """
-# import private_model_upload.sigmoid_train as s_t
+import private_model.sigmoid_train as s_t
 """
 #TO DO: incorporating sigmoid_train into the script?
 sigmoid_train.py requires an import and a private folder out of my caution for
@@ -25,26 +25,12 @@ available upon request. Functions that I use will be discussed as they come
 up.
 
 sigmoid_train.py trains a logistic regression model which I use to classify a
-restaraunt review as good or bad.
+restaurant review as good or bad.
 """
 # import sys
 # sys to later be appended in argument for text2glove.py directory
 # import os
 
-
-# https://stackoverflow.com/questions/15063936/csv-error-field-larger-than-field-limit-131072
-# in order to work around the error message I get with large csv fields:
-maxInt = sys.maxsize
-
-while True:
-    # decrease the maxInt value by factor 10
-    # as long as the OverflowError occurs.
-
-    try:
-        csv.field_size_limit(maxInt)
-        break
-    except OverflowError:
-        maxInt = int(maxInt/10)
 
 def sigmoid(x: np.ndarray):
     """
@@ -71,27 +57,14 @@ def load_feature_dictionary(file):
 
     Returns:
         A dictionary indexed by words, returning the corresponding glove
-        embedding dictionary(that will be converted into an np.ndarray).
+        embedding dictionary(that will be converted into a np.ndarray).
     """
     allwords = pd.read_csv(file, sep=',', on_bad_lines='warn',
                            engine='python', index_col=0, header=None, quoting=3,
                            escapechar='/')
     glove_map = allwords.to_dict(orient='index')
     return glove_map
-def predict_loss(
-    theta : np.ndarray,
-    X : np.ndarray
-) -> tuple[np.ndarray, np.ndarray]:
-    answer = np.zeros(len(X[:, 0]))
-    finalanswer = np.zeros(len(X[:, 0]), dtype=int)
-    x_i: np.ndarray = np.insert(X, 0, 1, axis=1) # prepending bias
-    for i in range(len(X)):
-        answer[i] = sigmoid(np.dot(theta, x_i[i]))
-        if answer[i] >= 0.5:
-            finalanswer[i] = 1
-        else:
-            finalanswer[i] = 0
-    return finalanswer, answer
+
 
 if __name__ == '__main__':
     # From CMU 10-601 starter code:
@@ -149,8 +122,23 @@ for l_temp in range(len(input_data)):
     Weighted average of GLoVe embeddings for the input string, weighted upon 
     word appearances.
     '''
-goodbad, likelihood = predict_loss(w_gb, features_gb)
+goodbad, likelihood = s_t.predict_loss(w_gb, features_gb)
+'''
+s_t.predict_loss(weights: np.ndarray, features: np.ndarray) is a function that
+calculates likelihood based on the sigmoid probability distribution 
+function. This function returns a label vector of ints equal in column 
+length to the feature vector, i.e. it is the same count of datapoints, 
+and likelihood with those same dimensions.  A copy of the 
+sigmoid function used in sigmoid_train.py is 
+included in this script. 
+
+Bias does not need to be in features, instead it is
+prepended to the features vector in the function itself. 
+A likelihood greater than 0.5 is labelled as 1, less is labelled 0.
+'''
 # TODO: improve this model, maybe boosted version?
 goodonly = input_data.iloc[([indices for indices in goodbad == 1])]
 if goodonly.size == 0:
     print('Empty "good" restaurant list')
+
+print('work-in-progress version')
